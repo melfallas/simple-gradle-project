163e9f863367a0838e4fe8c9936b7e404f6f9b69
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2022-09-20 14:56:33 -0600

    E. Adding basic gradle project.

---
+++ src/test/java/com/example/java/basic/sgp/SgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.basic.sgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
