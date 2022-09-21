b28ad484f04eac53121b8be2d6738949046e3409
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2022-09-20 21:59:40 -0600

    E. Fixing some build stage issues.

--- jenkins/build_stage/build.sh
+++ jenkins/build_stage/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Validación de parámetros
-#appDir=$1
-appDir=$PWD
-#targetDir=/target
-targetDir=/build/libs
-#appDir=java-app
+#APP_DIR=$1
+APP_DIR=$PWD
+#SOURCE_DIR=/target
+SOURCE_DIR=/build/libs
+TARGET_DIR=jenkins/build_stage/
+
+#APP_DIR=java-app
 echo "********************************"
 echo "*** Building jar with gradle ***"
 echo "********************************"
-if [ -z ${appDir} ]; then 
+if [ -z ${APP_DIR} ]; then 
 	echo "|"
 	echo "Not specified directory"
 	echo "|"
@@ -20,29 +22,29 @@ else
 	/usr/bin/id
 	echo "|"
 	echo "|"
-	echo "Current directory: ${appDir}"
+	echo "Current directory: ${APP_DIR}"
 	echo "|"
 	echo "Removing existing *.jar files ..."
 	echo "|"
-	echo "Dir original permissions ${appDir}:"
-	ls -l ${appDir}
+	echo "Dir original permissions ${APP_DIR}:"
+	ls -l ${APP_DIR}
 	echo "|"
-	echo "Getting *.jar files of ${appDir}${targetDir} directory ..."
-	ls -l ${appDir}${targetDir}
-	#echo "Borrando adicionales de directorio ${appDir}${targetDir}..."
+	echo "Getting *.jar files of ${APP_DIR}${SOURCE_DIR} directory ..."
+	ls -l ${APP_DIR}${SOURCE_DIR}
+	#echo "Borrando adicionales de directorio ${APP_DIR}${SOURCE_DIR}..."
 	#rm -f silo-0.0.1-SNAPSHOT.jar.original
-	#ls -l ${appDir}${targetDir}
+	#ls -l ${APP_DIR}${SOURCE_DIR}
 	# Copiar el jar	
 	echo "|"
 	echo "Copy *.jar files to jenkins/build directory ..."
-	#cp -f "$appDir"${targetDir}/silo-0.0.1-SNAPSHOT.jar jenkins/build/
-	cp -f "$appDir"${targetDir}/*.jar jenkins/build/
+	#cp -f "$APP_DIR"${SOURCE_DIR}/silo-0.0.1-SNAPSHOT.jar ${TARGET_DIR}
+	cp -f "$APP_DIR"${SOURCE_DIR}/*.jar ${TARGET_DIR}
 	echo ""
 	echo "######################"
 	echo "*** Building image ***"
 	echo "######################"
 	echo ""
-	cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+	cd ${TARGET_DIR} && docker-compose -f docker-compose-build.yml build --no-cache
 	echo "|"
 	echo "Removing pending images ..."
 	echo "docker rmi $(docker images -f "dangling=true" -q --no-trunc)"
